12d9786702e6b2cd107e2ce40f0828e59ce43ba2
JairKevinFG <jkfloresg@uni.pe>
2020-07-20 16:00:48 -0500

    levantando  components

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 
 class Form extends React.Component {
-  state = {}
-  handleChange = e => {
-    // console.log({
-    //   name: e.target.name,
-    //   value: e.target.value,
-    // });  
-      this.setState({
-        [e.target.name] : e.target.value
-      })
-  };
-
+  // state = {
+  //   'firstName':"kevin"
+  // }
+  // handleChange = e => {
+  //   // console.log({
+  //   //   name: e.target.name,
+  //   //   value: e.target.value,
+  //   // });  
+  //     this.setState({
+  //       [e.target.name] : e.target.value
+  //     })
+  // };
   handleClick = e => {
     console.log('Button was clicked');
   };
@@ -29,54 +30,54 @@ class Form extends React.Component {
           <div className="form-group">
             <h2>Nombre</h2>
             <input
-              onChange={this.handleChange}
+              onChange={this.props.onChange}
               className="form-control"
               type="text"
               name="firstName"
-              value={this.state.firstName}
+              value={this.props.formValues.firstName}
             />
           </div>
           <div className="form-group">
             <h2>Apellidos</h2>
             <input
-              onChange={this.handleChange}
+              onChange={this.props.onChange}
               className="form-control"
               type="text"
               name="lastName"
-              value={this.state.lastName}
+              value={this.props.formValues.lastName}
 
             />
           </div>
           <div className="form-group">
             <h2>Email</h2>
             <input
-              onChange={this.handleChange}
+              onChange={this.props.onChange}
               className="form-control"
               type="email"
               name="email"
-              value={this.state.email}
+              value={this.props.formValues.email}
             />
           </div>
 
           <div className="form-group">
             <h2>Especialidad</h2>
             <input
-              onChange={this.handleChange}
+              onChange={this.props.onChange}
               className="form-control"
               type="text"
               name="specialty"
-              value={this.state.specialty}
+              value={this.props.formValues.specialty}
             />
           </div>
 
           <div className="form-group">
             <h2>Twitter</h2>
             <input
-              onChange={this.handleChange}
+              onChange={this.props.onChange}
               className="form-control"
               type="text"
               name="twitter"
-              value={this.state.twitter}
+              value={this.props.formValues.twitter}
             />
           </div>
           <button onClick={this.handleClick} className="btn btn-primary">
--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -5,6 +5,26 @@ import Form from '../components/Form'
 import Card from '../components/Card'
 
 class Inicio extends React.Component {
+    state = {
+        form: {
+        firstName: '',
+        lastName: '',
+        email: '',
+        specialty: 'software developers',
+        twitter: ''
+        }
+}
+
+
+    handleChange = e => {
+        this.setState({
+            form: {
+                ...this.state.form,
+                [e.target.name] : e.target.value
+            }
+        })
+    }
+    
     render(){
         return (
             <h1>
@@ -16,10 +36,16 @@ class Inicio extends React.Component {
                  <div className="container">
                      <div className="row">
                      <div className="col-md-6">
-                             <Form/>
+                             <Form onChange = {this.handleChange} formValues = {this.state.form}/>
                          </div>
                          <div className="col-md-6">
-                             <Card avatar="https://s.gravatar.com/avatar/b07373a89159201f5c1e652dc3a2903b?s=80" twitter="jkevinfg" specialty="System Engineers"lastName ="Flores"firstName = "Kevin"/>
+                             <Card avatar="https://s.gravatar.com/avatar/b07373a89159201f5c1e652dc3a2903b?s=80"
+                              twitter= {this.state.form.twitter}
+                              specialty={this.state.form.specialty}
+                              lastName ={this.state.form.lastName}
+                              firstName = {this.state.form.firstName}
+                              email = {this.state.form.email}
+                              />
                          </div>
                         
                      </div>
